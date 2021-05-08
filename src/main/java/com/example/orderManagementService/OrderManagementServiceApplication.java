6921ef75edcff66374576f8310998730fc2e5dda
Ayush Kumar <ayush.kr.0008@gmail.com>
2021-05-08 10:45:44 +0530

    Added Hystrix,Zipkin,Cloud-gateway

--- src/main/java/com/example/orderManagementService/OrderManagementServiceApplication.java
+++ src/main/java/com/example/orderManagementService/OrderManagementServiceApplication.java
@@ -2,11 +2,12 @@ package com.example.orderManagementService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableCircuitBreaker
 public class OrderManagementServiceApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/orderManagementService/controller/OrderManagementController.java
+++ src/main/java/com/example/orderManagementService/controller/OrderManagementController.java
@@ -16,6 +16,7 @@ import com.example.orderManagementService.entity.FoodDto;
 import com.example.orderManagementService.entity.FoodOrder;
 import com.example.orderManagementService.exception.OrderManagementServiceException;
 import com.example.orderManagementService.service.FoodOrderService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 @RequestMapping("/api/v1/orders")
@@ -25,20 +26,38 @@ public class OrderManagementController {
 	private FoodOrderService foodOrderService;
 
 	@PutMapping("/{updateType}/{orderId}")
+	@HystrixCommand(fallbackMethod = "fallBackMethodOnResturantServiceFailure")
 	public ResponseEntity<?> updateOrder(@RequestBody FoodDto food, @PathVariable Long orderId, @PathVariable String updateType)
 			throws OrderManagementServiceException {
 		return new ResponseEntity<ResponseBody<FoodOrder>>(new ResponseBody<FoodOrder>(null,foodOrderService.updateOrder(food, orderId, updateType)), HttpStatus.OK);
 	}
 
 	@GetMapping("/{requestStatus}/{orderId}")
+	@HystrixCommand(fallbackMethod = "fallBackMethodOnResturantServiceFailure")
 	public ResponseEntity<?> cancelOrder(@PathVariable String requestStatus, @PathVariable long orderId)
 			throws OrderManagementServiceException {
 		return new ResponseEntity<ResponseBody<FoodOrder>>(new ResponseBody<FoodOrder>(null,foodOrderService.cancelOrder(orderId, requestStatus)), HttpStatus.OK);
 	}
 
 	@PostMapping("/{requestStatus}")
+	@HystrixCommand(fallbackMethod = "fallBackMethodOnResturantServiceFailure")
 	public ResponseEntity<?> placeOrder(@RequestBody FoodOrder foodOrder, @PathVariable String requestStatus)
 			throws OrderManagementServiceException {
 		return new ResponseEntity<ResponseBody<FoodOrder>>(new ResponseBody<FoodOrder>(null,foodOrderService.placeOrder(foodOrder, requestStatus)), HttpStatus.OK);
+	}
+	
+	public ResponseEntity<?> fallBackMethodOnResturantServiceFailure(@RequestBody FoodDto food, @PathVariable Long orderId, @PathVariable String updateType){
+		return new ResponseEntity<String>
+		("Resturant Search Service is Taking Too Long To Respond", HttpStatus.FAILED_DEPENDENCY);
+	}
+	
+	public ResponseEntity<?> fallBackMethodOnResturantServiceFailure(@PathVariable String requestStatus, @PathVariable long orderId){
+		return new ResponseEntity<String>
+		("Resturant Search Service is Taking Too Long To Respond", HttpStatus.FAILED_DEPENDENCY);
+	}
+	
+	public ResponseEntity<?> fallBackMethodOnResturantServiceFailure(@RequestBody FoodOrder foodOrder, @PathVariable String requestStatus){
+		return new ResponseEntity<String>
+		("Resturant Search Service is Taking Too Long To Respond", HttpStatus.FAILED_DEPENDENCY);
 	}
 }
